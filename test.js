d66042dbd70d3a92adfd1e5f90ebac21bcfeed12
Vivek <vivek@snapshopr.co>
2016-07-23 20:11:36 +0530

    code base structured

--- test.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-var artifacia = require("./artifacia");
-var client = artifacia.artifacia("dummy", "abcd1234")
-var result = artifacia.get_visual_recommendation(2709,function(result){console.log(result)});
